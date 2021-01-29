97e62f165bcb570abd0b8775f19c30e72374006e
Jackie Yuan <sy2938@columbia.edu>
2021-01-28 20:05:35 -0500

    finished ETL

---
+++ Lambdas/dummyResponse/lambda_function.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('I’m still under development. Please come back later.')
+    }
